acc17070ea9dd6b9d66ef48afd2ab54d3822cd36
hung <hungtranpro2@gmail.com>
2020-12-15 16:01:58 +0700

    implement todoform

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./App.scss";
+import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
 const App = () => {
@@ -17,9 +18,26 @@ const App = () => {
     newTodoList.splice(index, 1);
     setTodoList(newTodoList);
   };
+
+  const handleTodoFormSubmit = (formValues) => {
+    console.log("value", formValues);
+
+    // add new todo to current todo list
+
+    const newTodo = {
+      id: todoList.length + 1,
+      ...formValues,
+    };
+
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
+  };
+
   return (
     <div className="app">
       <h1>React Hooks - TodoList</h1>
+      <TodoForm onSubmit={handleTodoFormSubmit} />
       <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
